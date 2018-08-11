3d98c5ef3d7ffcf1b51d03ddd397bd92465ec32b
Prabhat Gautam <prabhatgautam@lftechnology.com>
2018-08-11 12:37:48 +0545

    Update friend query

--- src/db/queries/get_friends_list.js
+++ src/db/queries/get_friends_list.js
@@ -3,7 +3,7 @@ export function getFriendsListQuery(userId) {
   (
     SELECT
       f.id as friendship_id,
-      l.username as friend_name
+      l.username as name
       FROM friendships f
       INNER JOIN logins l on l.id = f.user_id_2
       WHERE user_id_1=${userId}
@@ -12,7 +12,7 @@ export function getFriendsListQuery(userId) {
   (
     SELECT
       f.id as friendship_id,
-      l.username as friend_name
+      l.username as name
       FROM friendships f
       INNER JOIN logins l on l.id = f.user_id_1
       WHERE user_id_2=${userId}
